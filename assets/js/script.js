127b771da2943b646e0ce0f2b7e211824e53befa
Lone1ne <ds@Dereks-Air.attlocal.net>
2023-05-23 20:01:28 -0400

    Implement question display and answer check
    - Created a newQuestion() function to dynamically load and display questions from an array, along with their answer choices as buttons on the page.
    - Added an event listener to detect button clicks and capture user's chosen answer.
    - Created a checkAnswer() function to compare user's answer with the correct one, incrementing score for correct answers and decreasing time for incorrect ones.
    - After each answered question, we update the display with the next question.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,109 @@
+//DOM references
 var titleElem = document.querySelector(".title");
 var descriptionElem = document.querySelector(".description");
-var startElem = document.querySelector("button");
+var startElem = document.querySelector(".start-button");
 var timeElem = document.querySelector(".time");
+
+//Declare Varibles
+//Create Questions array
+var questions = [
+  {
+    title: "How would you create a new object in JavaScript?",
+    choices: [
+      "var object = [];",
+      "var object = {};",
+      "var object = ();",
+      "var object = //;",
+    ],
+    answer: "var object = {};",
+  },
+  {
+    title: "Which is the correct way to declare a variable in JavaScript?",
+    choices: ["int x = 10;", "var x = 10;", "10 > x;", "x += 10;"],
+    answer: "var x = 10;",
+  },
+  {
+    title: "What keyword is used to define a function in JavaScript?",
+    choices: ["function", "method", "var", "const"],
+    answer: "function",
+  },
+  {
+    title: "Which is the correct way to create a function in JavaScript?",
+    choices: [
+      "function newFunction(){}",
+      "newFunction(){}",
+      "func = newFunction(){}",
+      "newFunction: function(){}",
+    ],
+    answer: "function newFunction(){}",
+  },
+  {
+    title: "Which method returns the length of a string?",
+    choices: ["length()", "size()", "index()", "None of the above"],
+    answer: "length()",
+  },
+  {
+    title: "Which of the following will correctly declare an array?",
+    choices: ["var arr[];", "var arr = [];", "array arr = [];", "arr{} = ();"],
+    answer: "var arr = [];",
+  },
+  {
+    title: "How do you add a comment in JavaScript?",
+    choices: [
+      "<!--This is a comment-->",
+      "# This is a comment",
+      "//This is a comment",
+      "''This is a comment",
+    ],
+    answer: "//This is a comment",
+  },
+];
+
+var timeLeft;
+var currentQuestionIndex = 0;
+var score = 0;
+var penalty = -10;
 
 titleElem.textContent = "Coding Quiz Challenge!";
 descriptionElem.textContent =
   "Try to answer the following code-related questions within the time limit. Keep in mind that incorrect answers will penalize your score/time by ten seconds!";
 startElem.textContent = "start quiz";
 
-var timeLeft;
+//Functions
+
+function newQuestion() {
+  //check if we run out of questions
+  if (currentQuestionIndex >= questions.length) {
+    // handle end of questions, stop timer, show results
+    return;
+  }
+  //get the current question
+  var currentQuestion = questions[currentQuestionIndex];
+  //display the current question
+  titleElem.textContent = currentQuestion.title;
+  //clear the description/ answer options
+  descriptionElem.innerHTML = "";
+
+  //display the current question choices as buttons
+  for (var i = 0; i < currentQuestion.choices.length; i++) {
+    var button = document.createElement("button");
+    button.setAttribute("class", "answer-button");
+    button.textContent = currentQuestion.choices[i];
+    descriptionElem.appendChild(button);
+  }
+  //move the next question for the next time through
+  currentQuestionIndex++;
+}
+
+function checkAnswer(choice) {
+  var correctAnswer = questions[currentQuestionIndex - 1].answer;
+  if (correctAnswer === choice) {
+    score++;
+  } else {
+    timeLeft = timeLeft + penalty;
+  }
+  newQuestion();
+}
 
 function timer() {
   timeLeft = 75;
@@ -19,12 +114,22 @@ function timer() {
     if (timeLeft === 0) {
       clearInterval(timerInterval);
       //do something
+      gameOver();
     }
   }, 1000);
 }
 
 function startQuiz() {
   timer();
+  newQuestion();
 }
 
+// Event Listeners
 startElem.addEventListener("click", startQuiz);
+
+descriptionElem.addEventListener("click", function (event) {
+  // Check if a button was clicked
+  if (event.target.className === "answer-button") {
+    checkAnswer(event.target.textContent);
+  }
+});
